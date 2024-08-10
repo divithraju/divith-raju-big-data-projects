662ed5981686d14786c3911babdcb0610e05b6f7
divith raju <118492176+divithraju@users.noreply.github.com>
2024-08-10 12:26:31 +0530

    Add files via upload

---
+++ ubuntu.sh
@@ -0,0 +1,56 @@
+
+nano df.sh
+ls
+ls-a
+ls /home/divithraju/Downloads
+cd /home/divithraju/Downloads
+cd /home/divithraju/Downloads/hadoop-3.3.6
+ls ~/Downloads
+ls ~/Pictures
+plocate tcs
+which hadoop
+free
+df
+du
+free -h
+top
+cp linux pyspark-1
+mv pyspark-1 linux
+rm pyspark-1
+rm -r pyspark-1
+mkdir Tata
+rmdir Tata
+cat
+head linux
+tail linux
+chmod 755 linux 
+chown pond linux
+chgrp linux network
+df -h
+du -sh Tata
+find Tata
+grep ls linux
+uptime
+whoami
+uname
+uname -a
+sudo apt upgrade
+sudo apt update
+sudo shutdown -h now
+sudo reboot
+sudo apt install package name
+sudo apt remove pacakge name
+touch linux
+ls -l
+ls -lh
+diff linux pyspark-1
+echo  90
+echo 'hkjkh'
+#Shell Script
+nano kl.sh
+echo 'type'
+echo 6785
+top
+./kl.sh
+chmod +x kl.sh
+
